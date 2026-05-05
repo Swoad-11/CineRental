52488fd879402c96bede50b44f64fe3a3ffe0612
Swoad-11 <toufiq11swoad@gmail.com>
2026-05-05 10:05:49 +0600

    fixing vercel issue

--- app/[lang]/movies/[id]/page.js
+++ app/[lang]/movies/[id]/page.js
@@ -1,38 +1,24 @@
 import Navbar from "../../components/Navbar";
-import Sidebar from "../../components/Sidebar";
-import MobileMenu from "../../components/MobileSidebar";
 import Footer from "../../components/Footer";
-import MovieList from "../../components/MovieList";
-import { getAllMovies } from "../movies";
+import MobileSidebar from "../../components/MobileSidebar";
+import Modal from "../../components/Modal";
+import MovieDetails from "../../components/MovieDetails";
+import { getMovieById } from "../movies";
 import { getDictionary } from "../dictionaries";
 
-const Home = async ({ params: { lang }, searchParams }) => {
+const MoviePage = async ({ params: { lang, id } }) => {
+  const movie = await getMovieById(id);
   const dictionary = await getDictionary(lang);
-  const filter = searchParams?.filter ?? "trending";
-
-  // Pass filter to getAllMovies so it can fetch/filter accordingly.
-  // If your movies function doesn't support filtering yet, see note below.
-  const movies = await getAllMovies(filter);
 
   return (
     <>
-      <Navbar mobileTrigger={<MobileMenu dictionary={dictionary} />} />
-
-      <main className="flex min-h-[calc(100vh-4rem)]">
-        {/* Desktop sidebar */}
-        <div className="hidden lg:block w-[218px] shrink-0 border-r border-white/6 pl-6 pr-2 pt-8">
-          <Sidebar lang={lang} searchParams={searchParams} />
-        </div>
-
-        {/* Content */}
-        <div className="flex-1 min-w-0 px-5 sm:px-8 pt-8 pb-12">
-          <MovieList lang={lang} movies={movies} filter={filter} />
-        </div>
-      </main>
-
+      <Navbar mobileTrigger={<MobileSidebar dictionary={dictionary} />} />
+      <Modal>
+        <MovieDetails id={id} lang={lang} />
+      </Modal>
       <Footer />
     </>
   );
 };
 
-export default Home;
+export default MoviePage;
