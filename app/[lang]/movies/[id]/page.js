a7dd5adfddc0270e403d377c373c8b174833a2d1
Swoad-11 <toufiq11swoad@gmail.com>
2026-05-05 10:52:21 +0600

    fix intercepting route imports

--- app/[lang]/movies/[id]/page.js
+++ app/[lang]/movies/[id]/page.js
@@ -1,7 +1,9 @@
-import Modal from "../../components/Modal";
-import MovieDetails from "../../components/MovieDetails";
+import Modal from "@/app/components/Modal";
+import MovieDetails from "@/app/components/MovieDetails";
+import { getDictionary } from "@/app/[lang]/dictionaries";
 
-const MovieModal = async ({ params: { id, lang } }) => {
+const MovieModal = async ({ params: { lang, id } }) => {
+  const dictionary = await getDictionary(lang);
   return (
     <Modal>
       <MovieDetails id={id} lang={lang} />
